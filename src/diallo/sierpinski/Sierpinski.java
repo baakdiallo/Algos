b3b3beee70f025f59cd414fcc6754c815779df6a
diallo <baakdiallo@gmail.com>
2015-08-15 23:53:25 -0400

    Completed Sierpinski Triangle and Square

--- src/diallo/sierpinski/Sierpinski.java
+++ src/diallo/sierpinski/Sierpinski.java
@@ -24,20 +24,33 @@ public class Sierpinski {
 		 * System.out.println("Please enter the number of iterations ..."); int
 		 * numIter = scan.nextInt();
 		 */
+
 		int x1 = 350;
 		int x2 = 100;
 		int x3 = 600;
 		int y1 = 50;
 		int y2 = 350;
-		int numIter = 3;
+		int numIterTriangle = 5;
 
 		JFrame frame = new JFrame("Sierpinski Triangle");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().add(
 				new SierpinskiTriangle(new Point(x1, y1), new Point(x2, y2),
-						new Point(x3, y2), numIter));
+						new Point(x3, y2), numIterTriangle));
 		frame.pack();
 		frame.setVisible(true);
+
+		int x = 100;
+		int y = 100;
+		int length = 400;
+		int numIterSquare = 5;
+
+		JFrame frame2 = new JFrame("Sierpinski Triangle");
+		frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame2.getContentPane().add(
+				new SierpinskiSquare(new Point(x, y), length, numIterSquare));
+		frame2.pack();
+		frame2.setVisible(true);
 	}
 
 }
--- src/diallo/sierpinski/SierpinskiSquare.java
+++ src/diallo/sierpinski/SierpinskiSquare.java
@@ -1,5 +1,8 @@
 package diallo.sierpinski;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.Polygon;
 import java.util.ArrayList;
@@ -7,8 +10,8 @@ import java.util.List;
 
 import javax.swing.JPanel;
 
-public class SierpinskiSquare extends JPanel{
-	
+public class SierpinskiSquare extends JPanel {
+
 	List<Polygon> polygons;
 
 	Point topLeft;
@@ -18,16 +21,68 @@ public class SierpinskiSquare extends JPanel{
 	int[] x = new int[n];
 	int[] y = new int[n];
 
-	int WIDTH = 700;
+	int WIDTH = 600;
 
-	Polygon square;
-	
-	public SierpinskiSquare(Point topLeft, int length, int numIter){
+	public SierpinskiSquare(Point topLeft, int length, int numIter) {
 		this.topLeft = topLeft;
 		this.length = length;
 		this.numIter = numIter;
-		
+
 		polygons = new ArrayList<Polygon>();
+
+		Point p2 = new Point(topLeft.x + length, topLeft.y);
+		Point p3 = new Point(topLeft.x + length, topLeft.y + length);
+		Point p4 = new Point(topLeft.x, topLeft.y + length);
+
+		setPreferredSize(new Dimension(WIDTH, WIDTH));
+
+		sierpinskify(numIter, topLeft, p2, p3, p4);
+	}
+
+	public void paintComponent(Graphics g) {
+		g.setColor(Color.BLUE);
+		for (Polygon p : polygons)
+			g.fillPolygon(p);
+	}
+
+	void sierpinskify(int numIter, Point topLeft, Point p2, Point p3, Point p4) {
+		if (numIter == 0)
+			return;
+		int third = (p2.x - topLeft.x) / 3;
+		Point newTopLeft = new Point(topLeft.x + third, topLeft.y + third);
+		Point newP2 = new Point(newTopLeft.x + third, newTopLeft.y);
+		Point newP3 = new Point(newTopLeft.x + third, newTopLeft.y + third);
+		Point newP4 = new Point(newTopLeft.x, newTopLeft.y + third);
+		x[0] = newTopLeft.x;
+		x[1] = newP2.x;
+		x[2] = newP3.x;
+		x[3] = newP4.x;
+		y[0] = newTopLeft.y;
+		y[1] = newP2.y;
+		y[2] = newP3.y;
+		y[3] = newP4.y;
+
+		Polygon square = new Polygon(x, y, 4);
+		polygons.add(square);
+
+		sierpinskify(numIter - 1, topLeft, new Point(newTopLeft.x, topLeft.y),
+				newTopLeft, new Point(topLeft.x, newTopLeft.y));
+		sierpinskify(numIter - 1, new Point(newTopLeft.x, topLeft.y),
+				new Point(newP2.x, p2.y), newP2, newTopLeft);
+		sierpinskify(numIter - 1, new Point(newP2.x, p2.y), p2, new Point(p2.x,
+				newP2.y), newP2);
+		sierpinskify(numIter - 1, newP2, new Point(p2.x, newP2.y), new Point(
+				p2.x, newP3.y), newP3);
+		sierpinskify(numIter - 1, newP3, new Point(p3.x, newP3.y), p3,
+				new Point(newP3.x, p3.y));
+		sierpinskify(numIter - 1, newP4, newP3, new Point(newP3.x, p3.y),
+				new Point(newP4.x, p4.y));
+		sierpinskify(numIter - 1, new Point(p4.x, newP4.y), newP4, new Point(
+				newP4.x, p4.y), p4);
+		sierpinskify(numIter - 1, new Point(topLeft.x, newTopLeft.y),
+				newTopLeft, newP4, new Point(topLeft.x, newP4.y));
+
+		repaint();
 	}
 
 }
