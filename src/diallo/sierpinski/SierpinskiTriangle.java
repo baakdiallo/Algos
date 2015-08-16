ac3d1d9ee15edf426567f6b9bfb3ea442ff8724d
diallo <baakdiallo@gmail.com>
2015-08-15 23:08:04 -0400

    Removed unnecessary imports

--- src/diallo/sierpinski/SierpinskiTriangle.java
+++ src/diallo/sierpinski/SierpinskiTriangle.java
@@ -3,10 +3,8 @@ package diallo.sierpinski;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.Polygon;
-import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.List;
 
