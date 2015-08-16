32ce6e49f72b839d3da7a825a9c18cc587dc7a08
diallo <baakdiallo@gmail.com>
2015-08-15 23:06:55 -0400

    Coded Sierpinski Triangle

--- src/diallo/sierpinski/Sierpinski.java
+++ src/diallo/sierpinski/Sierpinski.java
@@ -1,7 +1,6 @@
 package diallo.sierpinski;
 
 import java.awt.Point;
-import java.util.Scanner;
 
 import javax.swing.JFrame;
 
@@ -10,24 +9,33 @@ public class Sierpinski {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Scanner scan = new Scanner(System.in);
-		System.out
-				.println("Please enter the space-separated x values for the triangle ...");
-		String[] xS = scan.nextLine().split(" ");
-		System.out
-				.println("Please enter the space-separated x values for the triangle ...");
-		String[] yS = scan.nextLine().split(" ");
-		System.out.println("Please enter the number of iterations ...");
-		int numIter = scan.nextInt();
+		/*
+		 * Scanner scan = new Scanner(System.in);
+		 * 
+		 * System.out .println(
+		 * "Please enter the space-separated x values (three) for the triangle ..."
+		 * ); int x1 = scan.nextInt(); int x2 = scan.nextInt(); int x3 =
+		 * scan.nextInt();
+		 * 
+		 * System.out .println(
+		 * "Please enter the space-separated y values (two) for the triangle ..."
+		 * ); int y1 = scan.nextInt(); int y2 = scan.nextInt();
+		 * 
+		 * System.out.println("Please enter the number of iterations ..."); int
+		 * numIter = scan.nextInt();
+		 */
+		int x1 = 350;
+		int x2 = 100;
+		int x3 = 600;
+		int y1 = 50;
+		int y2 = 350;
+		int numIter = 3;
 
 		JFrame frame = new JFrame("Sierpinski Triangle");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().add(
-				new SierpinskiTriangle(new Point(Integer.valueOf(xS[0]),
-						Integer.valueOf(yS[0])), new Point(Integer
-						.valueOf(xS[0]), Integer.valueOf(yS[0])), new Point(
-						Integer.valueOf(xS[0]), Integer.valueOf(yS[0])),
-						numIter));
+				new SierpinskiTriangle(new Point(x1, y1), new Point(x2, y2),
+						new Point(x3, y2), numIter));
 		frame.pack();
 		frame.setVisible(true);
 	}
--- src/diallo/sierpinski/SierpinskiTriangle.java
+++ src/diallo/sierpinski/SierpinskiTriangle.java
@@ -1,25 +1,89 @@
 package diallo.sierpinski;
 
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Point;
+import java.awt.Polygon;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JPanel;
 
 public class SierpinskiTriangle extends JPanel {
+
+	List<Polygon> polygons;
 
 	Point p1;
 	Point p2;
 	Point p3;
 	int numIter;
+	int n = 3;
+	int[] x = new int[n];
+	int[] y = new int[n];
+
+	int WIDTH = 700;
+	int HEIGHT = 400;
+
+	Polygon triangle;
 
 	public SierpinskiTriangle(Point p1, Point p2, Point p3, int numIter) {
 		this.p1 = p1;
 		this.p2 = p2;
 		this.p3 = p3;
 		this.numIter = numIter;
-		this.paint();
+
+		polygons = new ArrayList<Polygon>();
+
+		initializeTriangle();
+
+		setPreferredSize(new Dimension(WIDTH, HEIGHT));
+
+		sierpinskify(numIter, this.p1, this.p2, this.p3);
 	}
 
-	public void paint(Graphics g) {
-		g.drawLine(5, 14, 123, 12);
+	public void paintComponent(Graphics g) {
+		g.setColor(Color.ORANGE);
+		for (Polygon p : polygons)
+			g.fillPolygon(p);
+	}
+
+	void sierpinskify(int numIter, Point one, Point two, Point three) {
+		// System.out.println(one.x + "," + one.y + ") : (" + two.x + "," +
+		// two.y + ") : (" + three.x + "," + three.y + ")");
+		if (numIter == 0)
+			return;
+		Point mid1 = new Point((one.x + two.x) / 2, (one.y + two.y) / 2);
+		Point mid2 = new Point((three.x + two.x) / 2, (three.y + two.y) / 2);
+		Point mid3 = new Point((one.x + three.x) / 2, (one.y + three.y) / 2);
+		x[0] = mid1.x;
+		x[1] = mid2.x;
+		x[2] = mid3.x;
+		y[0] = mid1.y;
+		y[1] = mid2.y;
+		y[2] = mid3.y;
+
+		triangle = new Polygon(x, y, 3);
+		polygons.add(triangle);
+
+		sierpinskify(numIter - 1, one, mid1, mid3);
+		sierpinskify(numIter - 1, mid1, two, mid2);
+		sierpinskify(numIter - 1, mid3, mid2, three);
+
+		repaint();
+	}
+
+	void initializeTriangle() {
+		x[0] = p1.x;
+		x[1] = p2.x;
+		x[2] = p3.x;
+		y[0] = p1.y;
+		y[1] = p2.y;
+		y[2] = p3.y;
+
+		triangle = new Polygon(x, y, 3);
 	}
 
 }
