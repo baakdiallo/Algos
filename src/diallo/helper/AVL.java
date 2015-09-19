d2173b87a5c174306c31eed778bab70fc0cf5e96
diallo <baakdiallo@gmail.com>
2015-09-19 16:12:04 -0500

    working on AVL trees

--- src/diallo/helper/AVL.java
+++ src/diallo/helper/AVL.java
@@ -1,7 +1,27 @@
-  package diallo.helper;
+package diallo.helper;
 
-public class AVL {
+public class AVL extends BinarySearchTree{
 
+	public AVL(BTNode root) {
+		super(root);
+		// TODO Auto-generated constructor stub
+	}
 	
+	public AVL(BTNode root, BinarySearchTree left, BinarySearchTree right) {
+		super(root, left, right);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void add(BTNode node){
+		if(this.root == null)
+			this.setRoot(node);
+		else{
+			
+		}
+	}
+	
+	public void remove(BTNode node){
+		
+	}
 	
 }
---
+++ src/diallo/tester/AVLTester.java
@@ -0,0 +1,16 @@
+package diallo.tester;
+
+import diallo.helper.AVL;
+import diallo.helper.BTNode;
+
+public class AVLTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		AVL avl = new AVL(new BTNode(5));
+		
+		
+	}
+
+}
